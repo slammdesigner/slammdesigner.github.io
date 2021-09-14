24a0f5fcb03314f874826ae81130175da36003ea
Chinmay Somani <chinmay.somani04@gmail.com>
2021-09-14 20:17:50 +0530

    added regex for more efficiency

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,8 +1,6 @@
 import subprocess
 import optparse
-
-
-
+import re
 
 
 
@@ -22,12 +20,30 @@ def get_args():
 		
 
 def change_mac(interface,new_mac):
-	print ("[+] Changing Mac Address to ->", new_mac)
+	print "[+] Changing Mac Address to ->", new_mac
 	subprocess.call(["ifconfig",interface,"down"])
 	subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
 	subprocess.call(["ifconfig",interface,"up"])
 	
+
+
+def get_current_mac(interface):
+	ifconfig_result=subprocess.check_output(["ifconfig",interface])	
+	current_mac=re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w',ifconfig_result)
+	
+	if current_mac:
+		return current_mac.group(0)
+	else:
+		print"[-] Could not find mac address"
+
 options=get_args()
+current_mac= get_current_mac(options.interface)
+
 change_mac(options.interface,options.new_mac)
 
+current_mac= get_current_mac(options.interface)
+if current_mac == options.new_mac:
+	print"[+] Mac address changed to " ,current_mac
+else:
+	print"[-] Could not change Mac address to " ,current_mac
 
