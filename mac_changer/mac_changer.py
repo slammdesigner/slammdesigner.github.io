4ea652069444ceaae0009fd89d13c1b51683dd02
Chinmay Somani <chinmay.somani04@gmail.com>
2021-09-14 17:39:59 +0530

    Updating decision making in script

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -10,18 +10,24 @@ def get_args():
 	parser=optparse.OptionParser()
 	parser.add_option("-i", "--interface", dest="interface",help="Interface for which mac address have to change")
 	parser.add_option("-m","--mac",dest="new_mac",help="Enter new mac address")
-	return parser.parse_args()
+	(options,arguments)=parser.parse_args()
+	if not options.interface:
+		parser.error("[-] please specify an interface, use --help for more info")
+	elif not options.new_mac:
+		parser.error("[-] Please specify a new mac address , use --help for more info")
+	
+	return options
 	
 
 		
 
 def change_mac(interface,new_mac):
-	print "[+] Changing Mac Address to ->", new_mac
+	print ("[+] Changing Mac Address to ->", new_mac)
 	subprocess.call(["ifconfig",interface,"down"])
 	subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
 	subprocess.call(["ifconfig",interface,"up"])
 	
-(options,arguments)=get_args()
+options=get_args()
 change_mac(options.interface,options.new_mac)
 
 
